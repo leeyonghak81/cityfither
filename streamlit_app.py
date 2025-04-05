c000e9ab80c16f9c7e4c24e9df361e81fee317ff
leeyonghak81 <bestmyself1981@gmail.com>
2025-04-05 15:30:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,14 +34,14 @@ else:
     # automatically at the bottom of the page.
     if prompt := st.chat_input("What is up?"):
 
-        # Store and display the current prompt.
+        # Store and display the current prompt. 
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
